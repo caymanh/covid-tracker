246bb5dfed08002fcbad6194d7d5819c899463ea
Cayman Heng <hengcayman@gmail.com>
2021-05-15 09:51:49 -0400

    imported API to App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import CountryPicker from './components/CountryPicker/CountryPicker';
 
 import styles from './App.css';
 
+import { fetchData } from './api/';
+
 class App extends React.Component {
+
+    state = {
+        data: {},
+    }
+    
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
     render() {
+        const {data} = this.state;
         return (
             <div className={styles.container}>
-                <Cards/>
+                <Cards data={data}/>
                 <CountryPicker/>
                 <Chart/>
             </div>
