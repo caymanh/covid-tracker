6519fb68761e39f4ddd25e2eccc078e18ad4b2bb
Cayman Heng <hengcayman@gmail.com>
2021-05-16 15:48:37 -0400

    updated stylesheets

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     state = {
         data: {},
+        country: '',
     }
     
     async componentDidMount() {
@@ -18,12 +19,17 @@ class App extends React.Component {
         this.setState({data: fetchedData});
     }
 
+    handleCountryChange = async (country) => {
+        //fetch the data
+        //set the state
+    }
+
     render() {
         const {data} = this.state;
         return (
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart/>
             </div>
         )
